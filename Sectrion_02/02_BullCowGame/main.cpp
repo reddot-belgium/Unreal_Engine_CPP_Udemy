c9be3e165ca0bbe5716ab9c05390e2dc44d9ab8b
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-01-06 21:31:59 +0100

    college 22

--- Sectrion_02/02_BullCowGame/main.cpp
+++ Sectrion_02/02_BullCowGame/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main() {
+	// voorstellen van het spel
 	constexpr int WORD_LENGTH = 5;
+
 	cout << "Welkom to Bulls an Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram i'm thinking of?" << endl;
+
+	// krijg een woord van de speler
+	string Guess = "";
+	cout << "Enter your guess: ";
+	cin >> Guess;
+	cout << endl;
+	// toon de input van de speler terug
+	cout << "Your guess was: " << Guess << endl;
+
 	return 0;
 }
